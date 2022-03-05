e5d604e237c64ee859af9f5022d4452eee1de6dc
asalas <67822688+andrewjsalas@users.noreply.github.com>
2022-03-05 12:54:59 -0800

    add-and-render-rows
    Created the logic to render table rows using mock data from a separate js file.  The table displays the album, artist, release date, and genre of each array item.
    
    Also created the logic to submit a new inline row albeit in a local piece of state.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,106 @@
 import './App.css';
-
-
+import mockData from './mockData';
+import { useState } from 'react';
+import { nanoid } from 'nanoid'
 
 function App() {
+  const [albumInfo, setAlbumInfo] = useState(mockData)
+  const [addAlbumData, setAddAlbumData] = useState({
+    album: '',
+    artist: '',
+    released: '',
+    genre: ''
+  })
 
+  // Assigns user form input to the corresponding name attributes 
+  // that is set in addAlbumData 
+  const handleAddFormChange = (e) => {
+    e.preventDefault()
+
+    const fieldName = e.target.getAttribute('name')
+    const fieldValue = e.target.value
+
+    const newFormData = { ...addAlbumData}
+    newFormData[fieldName] = fieldValue
+
+    setAddAlbumData(newFormData)
+  }
+
+  // Handles the submit logic of the form
+  const handleAddFormSubmit = (e) => {
+    e.preventDefault()
+
+    const newAlbumInfo = {
+      id: nanoid(), // Generates id to identify which albumData 
+      // is being edited or deleted
+      album: addAlbumData.album,
+      artist: addAlbumData.artist,
+      released: addAlbumData.released,
+      genre: addAlbumData.genre
+    }
+
+    // Create new album array to avoid mutating the state
+    const newAlbumInfoArray = [...albumInfo, newAlbumInfo]
+    setAlbumInfo(newAlbumInfoArray)
+  }
 
 
 
   return (
     <div className="app-container">
       <table>
-        
+        <thead>
+          <tr>
+            <th>Album</th>
+            <th>Artist</th>
+            <th>Released</th>
+            <th>Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          {albumInfo.map((albumInfo) => (
+            <tr>
+              <td>{albumInfo.album}</td>
+              <td>{albumInfo.artist}</td>
+              <td>{albumInfo.released}</td>
+              <td>{albumInfo.genre}</td>
+            </tr>
+          ))}        
+        </tbody>
       </table>
+
+      <h2>Add an album</h2>
+      <form onSubmit={handleAddFormSubmit}>
+        <input 
+          type='text' 
+          name='album' 
+          required='required' 
+          placeholder='Enter An Album' 
+          onChange={handleAddFormChange}
+        />
+        <input 
+          type='text' 
+          name='artist' 
+          required='required' 
+          placeholder='Enter An Artist' 
+          onChange={handleAddFormChange}
+        />
+        <input 
+          type='text' 
+          name='released' 
+          required='required' 
+          placeholder='Enter The Release' 
+          onChange={handleAddFormChange}
+        />
+        <input 
+          type='text' 
+          name='genre' 
+          required='required' 
+          placeholder='Enter The Genre' 
+          onChange={handleAddFormChange}
+        />
+        <button type='submit'>Add</button>
+      </form>
     </div>
   );
 }
---
+++ src/mockData.js
@@ -0,0 +1,25 @@
+const mockData = [
+  {
+    id: 1,
+    album: 'Doomicus Epicus Metallicus',
+    artist: 'Candlemass',
+    released: '1986',
+    genre: 'Metal'
+  },
+  {
+    id: 2,
+    album: 'Paranoid',
+    artist: 'Black Sabbath',
+    released: '1986',
+    genre: 'Metal'
+  },
+  {
+    id: 3,
+    album: 'Led Zeppelin I',
+    artist: 'Led Zeppelin',
+    released: '1969',
+    genre: 'Classic Rock'
+  }
+]
+
+export default mockData
